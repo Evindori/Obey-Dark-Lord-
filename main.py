921de718993447fa827e00af3390248164ea8ba5
Evindori <evindori@gmail.com>
2020-04-16 15:51:10 +0300

    Смерть работает нормально, звёздочки убраны

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import morph
 import knight
 import ground
 import os
-from pygame import *
-from ground import*
+from ground import *
 from hero import *
-from morph import*
-from knight import*
+from morph import *
+from knight import *
 
 height = 463
 width = 900
@@ -20,26 +19,28 @@ FILE_DIR = os.path.dirname(__file__)
 class Camera(object):
     def __init__(self, camera_func, width, height):
         self.camera_func = camera_func
-        self.state = Rect(0, 0, width, height)
-	
+        self.state = pygame.Rect(0, 0, width, height)
+
     def apply(self, target):
         return target.rect.move(self.state.topleft)
 
     def update(self, target):
         self.state = self.camera_func(self.state, target.rect)
-        
+
+
 def camera_configure(camera, target_rect):
     l, t, _, _ = target_rect
     _, _, w, h = camera
-    l, t = -l+width / 2, -t+ height / 2
+    l, t = -l + width / 2, -t + height / 2
 
     l = min(0, l)
-    l = max(-(camera.width-width), l)
-    t = max(-(camera.height-height), t)
+    l = max(-(camera.width - width), l)
+    t = max(-(camera.height - height), t)
 
-    return Rect(l, t, w, h)   
-        
-def load_level(n, level, morphs,entities,enemies, playerX, playerY):
+    return pygame.Rect(l, t, w, h)
+
+
+def load_level(n, level, morphs, entities, enemies, playerX, playerY):
     c_level = '%s/levels/' + str(n) + '.txt'
     levelfile = open(c_level % FILE_DIR)
     line = " "
@@ -65,10 +66,11 @@ def load_level(n, level, morphs,entities,enemies, playerX, playerY):
                 k = Knight(int(charecters[1]), int(charecters[2]))
                 entities.add(k)
                 enemies.add(k)
-    
+
+
 def death(hero, morphs, grave, enemies):
     for morph in morphs:
-        morph.restart()    
+        morph.restart()
     hero.restart()
     for g in grave:
         g.restart()
@@ -81,33 +83,35 @@ def screen_saver(done):
     started = False
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Obey Dark Lord")
-    bg = Surface((width, height))     
-    
+    bg = pygame.Surface((width, height))
+
     background_image = pygame.image.load("screensaver.jpg").convert()
     while not started and not done:
         for e in pygame.event.get():
             if e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE:
                 started = True
-            if e.type == QUIT: #event handling
-                done = True            
-            screen.blit(background_image, (0,0))
-        pygame.display.update()    
+            if e.type == QUIT:  # event handling
+                done = True
+            screen.blit(background_image, (0, 0))
+        pygame.display.update()
     return done
+
+
 def main(done):
     background_image = pygame.image.load("bg.png").convert()
-    
+
     for i in range(level_amount):
         level = []
         pygame.mixer.music.load(str(i) + '.mp3')
-        pygame.mixer.music.play(-1)    
+        pygame.mixer.music.play(-1)
         entities = pygame.sprite.Group()
-        grave = [] 
+        grave = []
         morphs = pygame.sprite.Group()
         enemies = pygame.sprite.Group()
         ending = pygame.sprite.Group()
         playerX = playerY = 100
-        load_level(i + 1, level, morphs,entities, enemies, playerX, playerY)
-        hero = Player(playerX,playerY)       
+        load_level(i + 1, level, morphs, entities, enemies, playerX, playerY)
+        hero = Player(playerX, playerY)
         entities.add(hero)
         x = 0
         y = 0
@@ -125,50 +129,49 @@ def main(done):
             y += gr_height
             x = 0
 
-        
         timer = pygame.time.Clock()
         left = right = up = 0
         morph_left = morph_right = morph_up = food = 0
-        total_level_width  = len(level[0])*gr_width
-        total_level_height = len(level)*gr_height
-        camera = Camera(camera_configure, total_level_width, total_level_height)         
+        total_level_width = len(level[0]) * gr_width
+        total_level_height = len(level) * gr_height
+        camera = Camera(camera_configure, total_level_width, total_level_height)
         while (not hero.levelcompleted) and (not done):
             timer.tick(30)
             if hero.alive:
                 for e in pygame.event.get():
-                    if e.type == QUIT: #event handling
+                    if e.type == QUIT:  # event handling
                         done = True
-                        
-                    if e.type == KEYDOWN and e.key == K_UP:#lord behavior
+
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:  # lord behavior
                         up = True
-                    if e.type == KEYDOWN and e.key == K_LEFT:
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
                         left = True
-                    if e.type == KEYDOWN and e.key == K_RIGHT:
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
                         right = True
-                    if e.type == KEYUP and e.key == K_UP:
+                    if e.type == pygame.KEYUP and e.key == pygame.K_UP:
                         up = False
-                    if e.type == KEYUP and e.key == K_RIGHT:
+                    if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
                         right = False
-                    if e.type == KEYUP and e.key == K_LEFT:
-                        left = False    
-    
-                    if e.type == KEYDOWN and e.key == K_w:#morphs behavior
+                    if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
+                        left = False
+
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_w:  # morphs behavior
                         morph_up = True
-                    if e.type == KEYDOWN and e.key == K_a:
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_a:
                         morph_left = True
-                    if e.type == KEYDOWN and e.key == K_d:
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_d:
                         morph_right = True
-                    if e.type == KEYUP and e.key == K_w:
+                    if e.type == pygame.KEYUP and e.key == pygame.K_w:
                         morph_up = False
-                    if e.type == KEYUP and e.key == K_d:
+                    if e.type == pygame.KEYUP and e.key == pygame.K_d:
                         morph_right = False
-                    if e.type == KEYUP and e.key == K_a:
-                        morph_left = False     
-                    if e.type == KEYDOWN and e.key == K_e:
+                    if e.type == pygame.KEYUP and e.key == pygame.K_a:
+                        morph_left = False
+                    if e.type == pygame.KEYDOWN and e.key == pygame.K_e:
                         food = True
-                    if e.type == KEYUP and e.key == K_e:
-                        food = False                       
-                screen.blit(background_image, (0,0))
+                    if e.type == pygame.KEYUP and e.key == pygame.K_e:
+                        food = False
+                screen.blit(background_image, (0, 0))
                 for morph in morphs:
                     time.delay(3)
                     morph.update(morph_left, morph_right, morph_up, food, grave, enemies)
@@ -179,23 +182,25 @@ def main(done):
                 for e in entities:
                     screen.blit(e.image, camera.apply(e))
                 pygame.display.update()
-                
+
                 if hero.rect.y > height:
                     hero.alive = False
                 if not hero.alive:
                     death(hero, morphs, grave, enemies)
-            
+
     return done
+
 
 def end(done):
     background_image = pygame.image.load("end.png").convert()
     while not done:
         for e in pygame.event.get():
             if e.type == QUIT:
-                done = True            
-            screen.blit(background_image, (0,0))
-        pygame.display.update()    
+                done = True
+            screen.blit(background_image, (0, 0))
+        pygame.display.update()
     pygame.quit()
+
 
 if __name__ == "__main__":
     pygame.init()
@@ -204,5 +209,5 @@ if __name__ == "__main__":
     bg = Surface((width, height))
     done = False
     done = screen_saver(done)
-    done = main(done)        
+    done = main(done)
     end(done)
